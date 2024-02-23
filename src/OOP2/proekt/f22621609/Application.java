1c406db75d71e02c78f320402a0905a9a10fbf28
Nurdzhan <s22621609@onlineedu.tu-varna.bg>
2024-02-23 18:39:58 +0200

    second commit test

--- src/OOP2/proekt/f22621609/Application.java
+++ src/OOP2/proekt/f22621609/Application.java
@@ -3,5 +3,6 @@ package OOP2.proekt.f22621609;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("sadad");
     }
 }
